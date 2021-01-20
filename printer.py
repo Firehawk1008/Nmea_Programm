6ec590f255beb166863387222a7a100c0b8aeb1f
Firehawk1008 <jan-pierre.wesche@stud.hs-hannover.de>
2021-01-20 12:04:06 +0100

    added a pretty printer, it now can print a table

--- printer.py
+++ printer.py
@@ -1,4 +1,5 @@
-import rich
+from rich.console import Console
+from rich.table import Table
 
 def dic_print(dic):
     for keys,values in dic.items():
@@ -7,3 +8,17 @@ def dic_print(dic):
         else:
             print (f"{keys} : /")
     print("")
+
+def pretty_dic_print(dic):
+    table = Table(title="NMEA-Daten")
+    table.add_column("Name", style="cyan", no_wrap=True)
+    table.add_column("Value", style="green")
+    for keys,values in dic.items():
+        if isinstance(values, str):
+            table.add_row(keys, values)
+        else:
+            table.add_row(keys, "No Value")
+    console = Console()
+    console.print(table)
+
+
