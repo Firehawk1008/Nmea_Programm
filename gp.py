600f28f75cb01fc2d94ebd40ef332b2f78e60cd3
Firehawk1008 <jan-pierre.wesche@stud.hs-hannover.de>
2021-01-20 12:03:36 +0100

    added def gll_data for the protocol

--- gp.py
+++ gp.py
@@ -7,15 +7,14 @@ import convert
 def gga(gpgga_data):
     gpgga_data.pop(0)
     
-    print("GPGGA:")
     gga_data = {
-        "Zeit" : convert.time(gpgga_data[0]) , 
-        "Breite" : convert.coordinate(gpgga_data[1], gpgga_data[2]) ,
-        "Länge" : convert.coordinate(gpgga_data[3], gpgga_data[4]) ,
-        "GPS-Qualität" : convert.gps_quality(gpgga_data[5]) ,
-        "Anzahl Sattelieten" : gpgga_data[6] ,
-        "Horizontal Dilution of Precision (HDOP)" : gpgga_data[7] ,
-        "Höhe" : gpgga_data[8] ,
+        "Zeit" : convert.time(gpgga_data[0]), 
+        "Breite" : convert.coordinate(gpgga_data[1], gpgga_data[2]),
+        "Länge" : convert.coordinate(gpgga_data[3], gpgga_data[4]),
+        "GPS-Qualität" : convert.gps_quality(gpgga_data[5]),
+        "Anzahl Sattelieten" : gpgga_data[6],
+        "HDOP" : gpgga_data[7],
+        "Höhe" : gpgga_data[8],
     #    "Höhendifferenz zwischen Ellipsoid und Geoid" : ""
     }
     return dict(gga_data)
@@ -25,12 +24,11 @@ def gga(gpgga_data):
 def gsa(gpgsa_data):
     gpgsa_data.pop(0)
 
-    print("GPGSA:")
     gsa_data = {
-        "Selection of 2D or 3D" : gpgsa_data[0],
+        "Selection of 2D or 3D" : convert.selection(gpgsa_data[0]),
         "3D fix" : gpgsa_data[1],
         "PRN" : convert.prn(gpgsa_data),
-        "PDOP" : gpgsa_data[-4],
+        #"PDOP" : gpgsa_data[-4],
         "HDOP" : gpgsa_data[-3],
         "VDOP" : gpgsa_data[-2],
     }
@@ -41,16 +39,28 @@ def gsa(gpgsa_data):
 def rmc(gprmc_data):
     gprmc_data.pop(0)
 
-    print("GPRMC:")
     rmc_data = {
-        "Zeit" : convert.time(gprmc_data[0]) ,
-        "Status" : convert.status(gprmc_data[1]) ,
-        "Breite" : convert.coordinate(gprmc_data[2], gprmc_data[3]) ,
-        "Länge" : convert.coordinate(gprmc_data[4], gprmc_data[5]) ,
+        "Zeit" : convert.time(gprmc_data[0]),
+        "Status" : convert.status(gprmc_data[1]),
+        "Breite" : convert.coordinate(gprmc_data[2], gprmc_data[3]),
+        "Länge" : convert.coordinate(gprmc_data[4], gprmc_data[5]),
         "Geschwindigkeit" : convert.speed(gprmc_data[6]),
         "Track angle" : f"{gprmc_data[7]}°", 
-        "Datum" : convert.date(gprmc_data[8]) ,
-        "Magnetische Vari." : f"{gprmc_data[9]}, W"
+        "Datum" : convert.date(gprmc_data[8]),
+        #"Magnetische Vari." : f"{gprmc_data[9]}, W"
     }
 
     return dict(rmc_data)
+
+# gets the raw gpgll data as an array/list and converts it to an dictonary with readable outputs
+# returns a dict
+def gll(gpgll_data):
+    gpgll_data.pop(0)
+    
+    gll_data = {
+        "Breite" : convert.coordinate(gpgll_data[0], gpgll_data[1]),
+        "Länge" : convert.coordinate(gpgll_data[2], gpgll_data[3]),
+        "Zeit" : convert.time(gpgll_data[4]),
+        "Status" : convert.status(gpgll_data[5])
+    }
+    return dict(gll_data)
