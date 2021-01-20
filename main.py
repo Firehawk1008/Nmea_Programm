89550145e87674810d13a66bfda5f80c48866d10
Firehawk1008 <jan-pierre.wesche@stud.hs-hannover.de>
2021-01-20 09:57:46 +0100

    implemeted the save option

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import convert
+from rich.prompt import Confirm
+import saver1
 import printer
 import gp
 import device_output
@@ -7,7 +9,7 @@ from device_output import device
 
 
 # sorts the data by the protocol type and converts it to a list, if it isn't already
-# returns None
+# returns a dict rdy to print
 def sort(raw_data):
     if not isinstance(raw_data,  list):
         raw_data = convert.to_a_list(raw_data)
@@ -29,13 +31,20 @@ def sort(raw_data):
         elif lines[0] == '$GLGSV':
             break
         else:
-            print(lines)
+            break
     return ""
 
 i = device()
+want_to_save = Confirm.ask("Wollen sie die Daten abspeichern?")
 while True:
-    dic = sort(i.output_str())
+    raw_protocol= i.output_str()
+    dic = sort(raw_protocol)
     printer.dic_print(dict(dic))
+
+    # saves the raw device output and the convertet data, if the user confirmed it at the start
+    if want_to_save == True:
+        saver1.save_raw(raw_protocol)
+        saver1.save(dict(dic))
 
 
 
