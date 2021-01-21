c981de6bc20301c5de490b34bd77df493de62a56
Firehawk1008 <jan-pierre.wesche@stud.hs-hannover.de>
2021-01-21 12:10:49 +0100

    doc, only summary print left other deleted

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ from device_output import device
 # Program disclaimer: Getting nmea protocol data, directly from the device and converts it to readable outputs
 
 
-# sorts the data by the protocol type and converts it to a list, if it isn't already
+# sorts the data by the protocol type and converts it to a list, if it isn't already, 
+#return "dont print" stands for all the protocols that are not needed
 # returns a dict rdy to print
 def sort(raw_data):
     if not isinstance(raw_data,  list):
@@ -27,8 +28,10 @@ def sort(raw_data):
             return "dont print"
         break
 
+# creates a dict that has all the relavent data from the last 12 protocols sended by the device
+# returns a dict 
 def summary():
-    final_dic = {
+    final_dict = {
         "Datum" : "", 
         "Zeit" : "",
         "Länge" : "", 
@@ -45,34 +48,40 @@ def summary():
         "Selection of 2D or 3D" : "",
         "3D fix" : "", 
     }
+    # the loop is giving the device output as a str into the sort func and then saves it into a dict
+    # the protocol has 6 lines, so in range(12) makes sure that it gets at least one line for the dict 
     for i in range(12):
-        temp_dic = sort(d.output_str())
-        if temp_dic != "dont print":
-            final_dic = {**final_dic, **temp_dic}
+        temp_dict = sort(dev.output_str())
+        # merges the dicts if they are relevent !the last input into the dict overrides the previous data!
+        if temp_dict != "dont print":
+            final_dict = {**final_dict, **temp_dict}
         else:
             pass
-    return dict(final_dic)
+    return dict(final_dict)
 
-d = device()
-want_summary = Confirm.ask("Wollen sie die Daten zusammengefasst?")
+dev = device()
+has_rich = Confirm.ask("Ist auf ihrem Gerät \"rich\" installiert?")
 want_to_save = Confirm.ask("Wollen sie die Daten abspeichern?")
+if want_to_save:
+    want_to_save_raw = Confirm.ask("Auch die roh Daten?")
+elif not want_to_save:
+    want_to_save_raw = False
+# the processing loop 
 while True:
-    if want_summary == False:
-        raw_protocol= d.output_str()
-        dic = sort(raw_protocol)
-        if dic == "dont print":
-            pass
-        else:
-            printer.dic_print(dict(dic))
-    else: 
-        sum_dic = dict(summary())
-        printer.pretty_dic_print(dict(sum_dic))
+    sum_dict = dict(summary())
+    if has_rich:
+        printer.pretty_dict_print(dict(sum_dict))
+    elif not has_rich:
+        printer.dict_print(dict(sum_dict))
 
     # saves the raw device output and the convertet data, if the user confirmed it at the start
-    if want_to_save and not want_summary:
+    if want_to_save_raw and want_to_save:
+        raw_protocol = dev.output_str()
         saver.save_raw(raw_protocol)
-        saver.save(dict(dic))
-    elif want_to_save and want_summary:
-        saver.save(dict(sum_dic))
+        saver.save_summary(dict(sum_dict))
+    elif want_to_save:
+        saver.save(dict(sum_dict))
+    else:
+        pass
 
 
