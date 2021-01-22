2ed4f8b4239bbd96ae2aa92d5a9f814e95192f4e
Firehawk1008 <jan-pierre.wesche@stud.hs-hannover.de>
2021-01-22 17:14:24 +0100

    Implemeted an Expetion for the input

--- device_output.py
+++ device_output.py
@@ -10,12 +10,16 @@ class device(object):
     def __init__(self):
         if os.geteuid() == 0:
             pass
+        # sudo rights are needed to use the serial input
         else:
-            print("Bitte gib das Rootpasswort ein")
             subprocess.call(['sudo', 'python3', *sys.argv])
             sys.exit()
-
-        self.ser = serial.Serial('/dev/ttyUSB0', 9600, 8, 'N', 1, timeout=1)
+        try:
+            self.ser = serial.Serial('/dev/ttyUSB0', 9600, 8, 'N', 1, timeout=1)
+        except Exception as err:
+            print("Bitte überprüfen sie ob ihr Gerät angeschlossen ist. \nVersuchen sie es anschließend erneut.")
+            quit()
+            
 
     # returns the output from the device in a single line as bytes
     def output(self):
